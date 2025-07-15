d50f0384425c391b80fbbed0c4d2fb9f3927dee1
Rahul9041 <135520296+Rahul9041@users.noreply.github.com>
2025-07-15 11:21:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 print("Its working")
 print("Rahul made change in it")
 print(3 + 5)
+
+def get_api_test():
+    import requests 
+    demo_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url=demo_url)
+    print(dir(response))     #it show its information
+    return response.json()   #it shows all data of url
+get_api_test()
